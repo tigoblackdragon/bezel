8b5de51e8a5d7bceb3e1be1f41fa25fc88918987
tigoblackdragon <engahmedmohammed04@gmail.com>
2024-05-29 22:30:23 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+  //hide the preloader on scroll
+  window.onload = function() {
+    var loader = document.getElementById('loader');
+    if (loader) {
+        loader.style.display = 'none';
+    }
+};
